f8c1e250a4c53ad9c5ae13e57f7e0ec5d496c843
Dakota Blackwell <dakota.blackwell@student.ufv.ca>
2020-11-09 14:32:43 -0800

    Copied Dev 2 classes to main app.

---
+++ app/src/main/java/com/aceteam/aceitflashcards/FlashCard.java
@@ -0,0 +1,474 @@
+import java.util.Set;
+import java.io.File;
+
+/**
+ * This class represents a flashcard. It contains a question and an answer, and may contain
+ * hints, incorrect answers, and tag. A collection of FlashCards is used in a Quiz.
+ * 
+ * @see Quiz
+ */
+public class FlashCard
+{
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  private String question;
+  private String answer;
+  private String hint;
+  private Set<String> wrongAnswers;
+  private Set<Tag> tags;
+  private Set<Quiz> quizzes;
+
+  //------------------------
+  // CONSTRUCTORS
+  //------------------------
+
+  /**
+   * The default constructor for the FlashCard class.
+   * 
+   * @param aQuestion A String with the question to show on the FlashCard.
+   * @param aAnswer A String with the answer to show on the FlashCard.
+   * @param aHint A String with the hint to show (if requested).
+   * @param aWrongAnswers A List of Strings containing wrong answers. This is to provide
+   * alternative answers when creating a Quiz.
+   * @param aTags A List of Tags. These tags are used for sorting FlashCards.
+   * 
+   * @see Tag
+   */
+  public FlashCard(String aQuestion, String aAnswer, String aHint, Set<String> aWrongAnswers, Set<Tag> aTags)
+  {
+    question = aQuestion;
+    answer = aAnswer;
+    hint = aHint;
+    wrongAnswers = aWrongAnswers;
+    tags = aTags;
+    quizzes = new HashSet<Quiz>();
+  }
+
+  /**
+   * A constructor for the FlashCard class.
+   * 
+   * @param aQuestion A String with the question to show on the FlashCard.
+   * @param aAnswer A String with the answer to show on the FlashCard.
+   */
+  public FlashCard(String aQuestion, String aAnswer){
+    FlashCard(aQuestion, aAnswer, "", new HashSet<String>(), new HashSet<Tag>());
+  }
+
+  /**
+   * A constructor for the FlashCard class.
+   * 
+   * @param aQuestion A String with the question to show on the FlashCard.
+   * @param aAnswer A String with the answer to show on the FlashCard.
+   * @param aHint A String with the hint to show (if requested).
+   */
+  public FlashCard(String aQuestion, String aAnswer, String aHint){
+    FlashCard(aQuestion, aAnswer, aHint, new HashSet<String>(), new HashSet<Tag>());
+  }
+  
+  /**
+   * A constructor for the FlashCard class.
+   * 
+   * @param aQuestion A String with the question to show on the FlashCard.
+   * @param aAnswer A String with the answer to show on the FlashCard.
+   * @param aHint A String with the hint to show (if requested).
+   * @param aWrongAnswers A List of Strings containing wrong answers. This is to provide
+   * alternative answers when creating a Quiz.
+   */
+  public FlashCard(String aQuestion, String aAnswer, String aHint, Set<String> aWrongAnswers){
+    FlashCard(aQuestion, aAnswer, aHint, aWrongAnswers, new HashSet<Tag>());
+  }
+
+  /**
+   * A constructor for the FlashCard class that imports information from a previously exported file.
+   * 
+   * @param file The file that contains the FlashCard information.
+   */
+  public FlashCard(File file){
+    // TODO: Code this.
+  }
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  /**
+   * Sets the question shown on the FlashCard.
+   * 
+   * @param aQuestion The question to show on the FlashCard.
+   * 
+   * @return True if the question was set; False otherwise.
+   */
+  public boolean setQuestion(String aQuestion)
+  {
+    boolean wasSet = false;
+    question = aQuestion;
+    wasSet = true;
+    return wasSet;
+  }
+
+  /**
+   * Sets the answer shown on the FlashCard.
+   * 
+   * @param aAnswer The answer to show on the FlashCard.
+   * 
+   * @return True if the answer was set; False otherwise.
+   */
+  public boolean setAnswer(String aAnswer)
+  {
+    boolean wasSet = false;
+    answer = aAnswer;
+    wasSet = true;
+    return wasSet;
+  }
+
+  /**
+   * Sets the hint shown on the FlashCard.
+   * 
+   * @param aHint The hint to show on the FlashCard.
+   * 
+   * @return True if the hint was set; False otherwise.
+   */
+  public boolean setHint(String aHint)
+  {
+    boolean wasSet = false;
+    hint = aHint;
+    wasSet = true;
+    return wasSet;
+  }
+
+  /**
+   * Adds a wrong answer to the FlashCard.
+   * 
+   * @param aWrongAnswer A wrong answer to add to the FlashCard.
+   * 
+   * @return True if the wrong answer was set; False otherwise.
+   */
+  public boolean addWrongAnswer(String aWrongAnswer)
+  {
+    boolean wasAdded = false;
+    wasAdded = wrongAnswers.add(aWrongAnswer);
+    return wasAdded;
+  }
+
+  /**
+   * Removes a wrong answer from the FlashCard.
+   * 
+   * @param aWrongAnswer The wrong answer to remove from the FlashCard.
+   * 
+   * @return True if the wrong answer was removed; False otherwise.
+   */
+  public boolean removeWrongAnswer(String aWrongAnswer)
+  {
+    boolean wasRemoved = false;
+    wasRemoved = wrongAnswers.remove(aWrongAnswer);
+    return wasRemoved;
+  }
+
+  /**
+   * Gets the question.
+   * 
+   * @return a String representing the question.
+   */
+  public String getQuestion()
+  {
+    return question;
+  }
+
+  /**
+   * Gets the answer.
+   * 
+   * @return a String representing the answer.
+   */
+  public String getAnswer()
+  {
+    return answer;
+  }
+
+  /**
+   * Gets the hint.
+   * 
+   * @return a String representing the hint.
+   */
+  public String getHint()
+  {
+    return hint;
+  }
+
+  /**
+   * Gets all of the wrong answers.
+   * 
+   * @return A Set of Strings containing all the wrong answers.
+   */
+  public Set<String> getWrongAnswers()
+  {
+    Set<Tag> newWrongAnswers = Collections.unmodifiableSet(wrongAnswers);
+    return newWrongAnswers;
+  }
+
+  /**
+   * Provides the number of wrong answers.
+   * 
+   * @return An integer containing the number of wrong answers.
+   */
+  public int numberOfWrongAnswers()
+  {
+    int number = wrongAnswers.size();
+    return number;
+  }
+
+  /**
+   * Checks if there are currently any wrong answers.
+   * 
+   * @return True if there are one or more wrong answers. False otherwise.
+   */
+  public boolean hasWrongAnswers()
+  {
+    boolean has = wrongAnswers.size() > 0;
+    return has;
+  }
+
+  /**
+   * Checks if there is a specific wrong answer.
+   * 
+   * @param aWrongAnswer A string with a wrong answer to look for.
+   * 
+   * @return True of the provided wrong answer is present. False otherwise.
+   */
+  public boolean hasWrongAnswer(String aWrongAnswer)
+  {
+    boolean has = wrongAnswers.contains(aWrongAnswer);
+    return has;
+  }
+
+  /**
+   * Gets all of the Tags.
+   * 
+   * @return A Set of all the Tags.
+   * 
+   * @see Tag
+   */
+  public Set<Tag> getTags()
+  {
+    Set<Tag> newTags = Collections.unmodifiableSet(tags);
+    return newTags;
+  }
+
+  /**
+   * Provides the number of Tags.
+   * 
+   * @return An integer containing the number of Tags.
+   */
+  public int numberOfTags()
+  {
+    int number = tags.size();
+    return number;
+  }
+
+  /**
+   * Checks if there are currently any Tags.
+   * 
+   * @return True if there are one or more Tags. False otherwise.
+   */
+  public boolean hasTags()
+  {
+    boolean has = tags.size() > 0;
+    return has;
+  }
+
+  /**
+   * Checks if there is a specific Tag.
+   * 
+   * @param aTag A specific Tag to look for.
+   * 
+   * @return True of the provided Tag is present. False otherwise.
+   * 
+   * @see Tag
+   */
+  public boolean hasTag(Tag aTag)
+  {
+    boolean has = tags.contains(aTag);
+    return has;
+  }
+  
+  
+  /**
+   * Adds a Tag to the FlashCard.
+   * 
+   * @param aTag The Tag to add to the FlashCard.
+   * 
+   * @return True if the Tag was added successfully. False otherwise.
+   * 
+   * @see Tag
+   */
+  public boolean addTag(Tag aTag)
+  {
+    if (tags.contains(aTag)) { return false; }
+
+    if (tags.add(aTag))
+    {
+      aTag.addFlashCard(this);
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Removes a Tag from the FlashCard.
+   * 
+   * @param aTag The Tag to remove from the FlashCard.
+   * 
+   * @return True of the Tag was removed successfully. False otherwise.
+   * 
+   * @see Tag
+   */
+  public boolean removeTag(Tag aTag)
+  {
+    if (!tags.contains(aTag)) { return false; }
+
+    if (tags.remove(aTag))
+    {
+      aTag.removeFlashCard(this);
+      return true;
+    }
+    return false;
+  }
+
+    /**
+   * Gets all of the Quizzes.
+   * 
+   * @return A Set of all the Quizzes.
+   * 
+   * @see Quiz
+   */
+  public Set<Quiz> getQuizzes()
+  {
+    Set<Quiz> newQuizzes = Collections.unmodifiableSet(quizzes);
+    return newQuizzes;
+  }
+
+  /**
+   * Provides the number of Quizzes.
+   * 
+   * @return An integer containing the number of Quizzes.
+   */
+  public int numberOfQuizzes()
+  {
+    int number = quizzes.size();
+    return number;
+  }
+
+  /**
+   * Checks if there are currently any Quizzes.
+   * 
+   * @return True if there are one or more Quizzes. False otherwise.
+   */
+  public boolean hasQuizzes()
+  {
+    boolean has = quizzes.size() > 0;
+    return has;
+  }
+
+  /**
+   * Checks if there is a specific Quiz.
+   * 
+   * @param aQuiz A specific Quiz to look for.
+   * 
+   * @return True of the provided Quiz is present. False otherwise.
+   * 
+   * @see Quiz
+   */
+  public boolean hasQuiz(Tag aQuiz)
+  {
+    boolean has = quizzes.contains(aQuiz);
+    return has;
+  }
+  
+  
+  /**
+   * Adds a Quiz to the FlashCard.
+   * 
+   * @param aQuiz The Quiz to add to the FlashCard.
+   * 
+   * @return True if the Quiz was added successfully. False otherwise.
+   * 
+   * @see Quiz
+   */
+  public boolean addQuiz(Quiz aQuiz)
+  {
+    if (quizzes.contains(aQuiz)) { return false; }
+
+    if (quizzes.add(aQuiz))
+    {
+      aQuiz.addFlashCard(this);
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Removes a Quiz from the FlashCard.
+   * 
+   * @param aQuiz The Quiz to remove from the FlashCard.
+   * 
+   * @return True of the Quiz was removed successfully. False otherwise.
+   * 
+   * @see Quiz
+   */
+  public boolean removeQuiz(Tag aQuiz)
+  {
+    if (!quizzes.contains(aQuiz)) { return false; }
+
+    if (quizzes.remove(aQuiz))
+    {
+      aQuiz.removeFlashCard(this);
+      return true;
+    }
+    return false;
+  }
+
+
+  /**
+   * Removes every Tag and Quiz from the FlashCard, and
+   * removes this FlashCard from the FlashCard list for each
+   * Tag and Quiz.
+   */
+  public void delete()
+  {
+    Iterator<Tag> i = tags.iterator();
+    while (i.hasNext()) {
+      Tag temp = i.next();
+      temp.removeFlashCard(this);
+      i.remove();
+    }
+    Iterator<Quiz> j = quizzes.iterator();
+    while (j.hasNext()) {
+      Quiz temp = j.next();
+      temp.removeFlashCard(this);
+      j.remove();
+    }
+  }
+
+  /**
+   * Exports the FlashCard to a File.
+   * 
+   * @return a File object with the exported FlashCard.
+   */
+  public File export(){
+    // TODO: Code this.
+  }
+
+  /**
+   * Returns a text representation of the FlashCard.
+   * @return A String representing the FlashCard.
+   */
+  public String toString()
+  {
+    return super.toString() + "["+
+            "question" + ":" + getQuestion()+ "," +
+            "answer" + ":" + getAnswer()+ "," +
+            "hint" + ":" + getHint()+ "]";
+  }
+}
---
+++ app/src/main/java/com/aceteam/aceitflashcards/Quiz.java
@@ -0,0 +1,303 @@
+import java.util.Set;
+import java.io.File;
+
+/** 
+ * The Quiz class is a collection of FlashCards that tests the user on
+ * their knowledge of a topic.
+ * 
+ * @see FlashCard
+ */
+public class Quiz
+{
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  private String name;
+  private int numQuestionsToShow;
+  private Set<FlashCard> flashCards;
+
+  //------------------------
+  // CONSTRUCTORS
+  //------------------------
+
+  /** 
+   * Constructs a Quiz with the specified name, the number of questions to show,
+   * and the set containing the FlashCards.
+   * 
+   * @param aName The name of the Quiz.
+   * @param aNumQuestionsToShow The number of questions to show on the quiz. Must be
+   * less than or equal to the number of FlashCards.
+   * @param aFlashCards A Set containing the FlashCards used in the Quiz.
+   */
+  public Quiz(String aName, int aNumQuestionsToShow, Set<FlashCard> aFlashCards)
+  {
+    name = aName;
+    numQuestionsToShow = aNumQuestionsToShow;
+    flashCards = aFlashCards;
+
+    // Quick sanity check on the max and min number of quiz questions.
+    if (numQuestionsToShow < 0) {
+      numQuestionsToShow = 0;
+    } else if (numQuestionsToShow > flashCards.size()) {
+      numQuestionsToShow = flashCards.size();
+    }
+  }
+
+  /** 
+   * Constructs a Quiz with the specified name using the provided set of FlashCards.
+   * 
+   * @param aName The name of the Quiz.
+   * @param aFlashCards A Set containing the FlashCards used in the Quiz.
+   */
+  public Quiz(String aName, Set<FlashCard> aFlashCards)
+  {
+    Quiz(aName, aFlashCards.size(), aFlashCards);
+  }
+
+  /** 
+   * Constructs a Quiz with the specified name using the set of FlashCards
+   * with the specified Tag.
+   * 
+   * @param aName The name of the Quiz.
+   * @param aTag A Tag relating to FlashCards.
+   */
+  public Quiz(String aName, Tag aTag)
+  {
+    Set<FlashCard> aFlashCards = new HashSet<FlashCard>(aTag.getFlashCards());
+    Quiz(aName, aFlashCards.size(), aFlashCards);
+  }
+
+  /** 
+   * Constructs a Quiz with the specified name.
+   * 
+   * @param aName The name of the Quiz.
+   */
+  public Quiz(String aName)
+  {
+    Quiz(aName, 0, new HashSet<FlashCard>());
+  }
+
+  /**
+   * A constructor for the Quiz class that imports information from a previously exported file.
+   * 
+   * @param file The file that contains the Quiz information.
+   */
+  public Quiz(File file){
+    // TODO: Code this.
+  }
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  /**
+   * Changes the name of the Quiz.
+   * 
+   * @param aName The new name of the Quiz.
+   * 
+   * @return True if the name was changed. False otherwise.
+   */
+  public boolean setName(String aName)
+  {
+    boolean wasSet = false;
+    name = aName;
+    wasSet = true;
+    return wasSet;
+  }
+
+  /**
+   * Sets the number of questions to show. Must be at least 0 and less than
+   * or equal to the number of FlashCards.
+   * 
+   * @param aNumQuestionsToShow The number of questions.
+   * 
+   * @return True if the value was set. False otherwise.
+   */
+  public boolean setNumQuestionsToShow(int aNumQuestionsToShow)
+  {
+    boolean wasSet = false;
+    numQuestionsToShow = aNumQuestionsToShow;
+    wasSet = true;
+    return wasSet;
+  }
+
+  /**
+   * Returns the name of the Quiz.
+   * 
+   * @return a String representing the name.
+   */
+  public String getName()
+  {
+    return name;
+  }
+
+  /**
+   * Returns the number of questions to show for the Quiz.
+   * 
+   * @return An integer containing the number of questions to show.
+   */
+  public int getNumQuestionsToShow()
+  {
+    return numQuestionsToShow;
+  }
+
+  /**
+   * Gets the set of FlashCards.
+   * 
+   * @return The set of FlashCards.
+   * 
+   * @see FlashCard
+   */
+  public Set<FlashCard> getFlashCards()
+  {
+    Set<FlashCard> newFlashCards = Collections.unmodifiableSet(flashCards);
+    return newFlashCards;
+  }
+
+  /**
+   * Gets the number of FlashCards with this Tag.
+   * 
+   * @return An integer with the number of FlashCards.
+   */
+  public int numberOfFlashCards()
+  {
+    int number = flashCards.size();
+    return number;
+  }
+
+  /**
+   * Checks if the set has FlashCards in it.
+   * 
+   * @return True if the set has at least one FlashCard in it. False otherwise.
+   */
+  public boolean hasFlashCards()
+  {
+    boolean has = flashCards.size() > 0;
+    return has;
+  }
+
+  /**
+   * Checks if there is a specific FlashCard.
+   * 
+   * @param aFlashCard The FlashCard to check.
+   * 
+   * @return True if the FlashCard is present. False otherwise.
+   * 
+   * @see FlashCard
+   */
+  public boolean hasFlashCard(FlashCard aFlashCard)
+  {
+    boolean has = flashCards.contains(aFlashCard);
+    return has;
+  }
+
+
+  /**
+   * Adds a FlashCard to this Quiz.
+   * 
+   * @param aFlashCard The FlashCard to add.
+   * 
+   * @return True if the FlashCard was added successfully. False otherwise.
+   * 
+   * @see FlashCard
+   */
+  public boolean addFlashCard(FlashCard aFlashCard)
+  {
+    if (flashCards.contains(aFlashCard)) { return false; }
+
+    if (flashCards.add(aFlashCard))
+    {
+      aFlashCard.addQuiz(this);
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Adds FlashCards to this Quiz by Tag.
+   * 
+   * @param aTag The Tag related to the FlashCards to add.
+   * 
+   * @return True if the FlashCards were added successfully. False otherwise.
+   * 
+   * @see FlashCard
+   */
+  public boolean addFlashCardsByTag(Tag aTag)
+  {
+    boolean added = false;
+    for (FlashCard card : aTag.getFlashCards()) {
+      boolean test = this.addFlashCard(card);
+      if (test) {
+        added = true;
+      }
+    }
+    return added;
+  }
+
+  /**
+   * Removes a FlashCard from this Quiz.
+   * 
+   * @param aFlashCard The FlashCard to remove.
+   * 
+   * @return True if the FlashCard was removed. False otherwise.
+   * 
+   * @see FlashCard
+   */
+  public boolean removeFlashCard(FlashCard aFlashCard)
+  {
+    if (!flashCards.contains(aFlashCard)) { return false; }
+
+    if (flashCards.remove(aFlashCard))
+    {
+      aFlashCard.removeQuiz(this);
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Removes every FlashCard from the Quiz.
+   */
+  public void delete()
+  {
+    Iterator<FlashCard> i = flashCards.iterator();
+    while (i.hasNext()) {
+      FlashCard temp = i.next();
+      temp.removeQuiz(this);
+      i.remove();
+    }
+  }
+
+  /**
+   * Exports the Quiz to a File.
+   * 
+   * @return a File object with the exported Quiz.
+   */
+  public File export(){
+    // TODO: Code this.
+  }
+
+  /**
+   * Returns a text representation of the Quiz.
+   * @return A String representing the Quiz.
+   */
+  public String toString()
+  {
+    String s = super.toString() + "["+
+            "name" + ":" + getName()+ "," +
+            "numQuestionsToShow" + ":" + getNumQuestionsToShow()+ ", {";
+    
+    Iterator<FlashCard> i = flashCards.iterator();
+    while (i.hasNext()) {
+      FlashCard temp = i.next();
+      s += temp.toString();
+      if (i.hasNext()) {
+        s += ", ";
+      }
+    }
+    s += "} ]";
+    return s;
+  }
+}

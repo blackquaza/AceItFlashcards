e474d9043e20ae3dc73fe0d04bdd216d08d03f94
Dakota Blackwell <dakota.blackwell@student.ufv.ca>
2020-12-01 15:38:10 -0800

    Figured out how to set text via Java.

--- app/src/main/java/com/aceteam/aceitflashcards/Fragment_FlashCardVertical.java
+++ app/src/main/java/com/aceteam/aceitflashcards/Fragment_FlashCardVertical.java
@@ -4,10 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
 
 public class Fragment_FlashCardVertical extends Fragment {
 
@@ -22,7 +22,8 @@ public class Fragment_FlashCardVertical extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
+        TextView box = view.findViewById(R.id.flashcard_vertical_textbox);
+        box.setText("Test text");
         /*
         view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
             @Override

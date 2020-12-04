7b6e9077cab275c9d12ee5ff3e0aee7bf9b72f9b
Dakota Blackwell <dakota.blackwell@student.ufv.ca>
2020-12-03 17:36:30 -0800

    Added Navdeep's input boxes to new page for FlashCard creation.

---
+++ app/src/main/java/com/aceteam/aceitflashcards/Fragment_CreateFlashCard.java
@@ -0,0 +1,35 @@
+package com.aceteam.aceitflashcards;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+public class Fragment_CreateFlashCard extends Fragment {
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_createflashcard, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.createflashcard_back).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(Fragment_CreateFlashCard.this)
+                        .navigate(R.id.action_fragment_CreateFlashCard_to_fragment_FlashCardList);
+            }
+        });
+    }
+}

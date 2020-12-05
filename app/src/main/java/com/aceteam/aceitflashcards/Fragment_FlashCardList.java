9a2b63aec86e1ab741f9abe6dabd1717d0e076b3
Dakota Blackwell <dakota.blackwell@student.ufv.ca>
2020-12-05 01:39:54 -0800

    Made the card list look good (or better anyways).

--- app/src/main/java/com/aceteam/aceitflashcards/Fragment_FlashCardList.java
+++ app/src/main/java/com/aceteam/aceitflashcards/Fragment_FlashCardList.java
@@ -1,16 +1,26 @@
 package com.aceteam.aceitflashcards;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+
+import com.google.android.material.card.MaterialCardView;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -27,6 +37,7 @@ public class Fragment_FlashCardList extends Fragment {
         return inflater.inflate(R.layout.fragment_flashcard_list, container, false);
     }
 
+    @SuppressWarnings("deprecation")
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
@@ -53,7 +64,7 @@ public class Fragment_FlashCardList extends Fragment {
             cardList.add(FlashCard.importFlash(cardFile));
         }
 
-        LinearLayout layout = (LinearLayout) view.findViewById(R.id.flashcardlist_cardlayout);
+        LinearLayout layout = view.findViewById(R.id.flashcardlist_cardlayout);
 
         for (FlashCard card : cardList) {
             if (card == null) continue;
@@ -61,7 +72,27 @@ public class Fragment_FlashCardList extends Fragment {
             text.setText(card.getQuestion());
             text.setPadding(10, 10, 10, 10);
 
-            text.setOnClickListener(new View.OnClickListener() {
+            MaterialCardView cardView = new MaterialCardView(getContext());
+            cardView.setStrokeColor(Color.BLACK);
+            cardView.setStrokeWidth(1);
+            cardView.setRadius(10);
+
+            // Fancy stuff here to get the actual height of the screen so I
+            // can scale cards to the proper size. Uses newly deprecated code
+            // (as in, deprecated in Android 11), so we're not using the new
+            // code for backwards compatibility.
+            Point s = new Point();
+            ((WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE))
+                    .getDefaultDisplay().getSize(s);
+
+            LinearLayout.LayoutParams p = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    s.y / 4
+            );
+            p.setMargins(10, 10, 10, 10);
+            cardView.setLayoutParams(p);
+
+            cardView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
 
@@ -73,7 +104,8 @@ public class Fragment_FlashCardList extends Fragment {
                 }
             });
 
-            layout.addView(text);
+            cardView.addView(text);
+            layout.addView(cardView);
 
         }
     }

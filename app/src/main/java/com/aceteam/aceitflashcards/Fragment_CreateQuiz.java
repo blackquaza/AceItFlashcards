8e9f48bd021cbdeb145686caebbadce7089ef579
Alex <alex.paterson@student.ufv.ca>
2020-12-05 18:04:59 -0800

    Create quiz crashes? :(

--- app/src/main/java/com/aceteam/aceitflashcards/Fragment_CreateQuiz.java
+++ app/src/main/java/com/aceteam/aceitflashcards/Fragment_CreateQuiz.java
@@ -1,20 +1,28 @@
 package com.aceteam.aceitflashcards;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.Space;
+import android.widget.TextView;
 
 import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.google.android.material.card.MaterialCardView;
+
 import java.io.File;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Fragment_CreateQuiz extends Fragment {
 
@@ -22,15 +30,16 @@ public class Fragment_CreateQuiz extends Fragment {
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
-    ) {
+    )
+    {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_createflashcard, container, false);
+        return inflater.inflate(R.layout.fragment_createquiz , container, false);
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.createflashcard_back).setOnClickListener(new View.OnClickListener() {
+        view.findViewById(R.id.createquiz_back).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(Fragment_CreateQuiz.this)
@@ -41,48 +50,68 @@ public class Fragment_CreateQuiz extends Fragment {
         OnBackPressedCallback callback = new OnBackPressedCallback(true) {
             @Override
             public void handleOnBackPressed() {
-                view.findViewById(R.id.createflashcard_back).performClick();
+                view.findViewById(R.id.createquiz_back).performClick();
             }
         };
 
 
 
+        List<FlashCard> cardList = new ArrayList<>();
 
+        File folder = new File(getContext().getFilesDir(), "flashcards");
+        for (File cardFile : folder.listFiles()) {
+            cardList.add(FlashCard.importFlash(cardFile));
+      }
+        LinearLayout layout = view.findViewById(R.id.quizlist_cardlayout );
 
-        requireActivity().getOnBackPressedDispatcher().addCallback(getViewLifecycleOwner(), callback);
+        // Fancy stuff here to get the actual height of the screen so I
+        // can scale cards to the proper size. Uses newly deprecated code
+        // (as in, deprecated in Android 11), so we're not using the new
+        // code for backwards compatibility.
+        Point s = new Point();
+        ((WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE))
+                .getDefaultDisplay().getSize(s);
+        int cardHeight = s.y / 4;
 
-        view.findViewById(R.id.createflashcard_save).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        LinearLayout.LayoutParams p = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                cardHeight
+        );
+        p.setMargins(10, 10, 10, 10);
 
-                EditText qText = getActivity().findViewById(R.id.createflashcard_questioninput);
-                String question = qText.getText().toString().trim();
-                EditText aText = getActivity().findViewById(R.id.createflashcard_answerinput);
-                String answer = aText.getText().toString().trim();
-                EditText hText = getActivity().findViewById(R.id.createflashcard_hintinput);
-                String hint = hText.getText().toString().trim();
-                Set<String> wrongAnswers = new HashSet<String>();
-                Set<Tag> tags = new HashSet<Tag>();
+        for (FlashCard card : cardList) {
+            if (card == null) continue;
+            TextView text = new TextView(getContext());
+            text.setText(card.getQuestion());
+            text.setPadding(10, 10, 10, 10);
 
-                if (question.isEmpty()) {
-                    String e = getResources().getString(R.string.question_needed);
-                    Toast.makeText(getContext(), e, Toast.LENGTH_SHORT).show();
-                } else if (answer.isEmpty()) {
-                    String e = getResources().getString(R.string.answer_needed);
-                    Toast.makeText(getContext(), e, Toast.LENGTH_SHORT).show();
-                } else {
+            MaterialCardView cardView = new MaterialCardView(getContext());
+            cardView.setStrokeColor(Color.BLACK);
+            cardView.setStrokeWidth(1);
+            cardView.setRadius(10);
+            cardView.setLayoutParams(p);
 
-                    FlashCard card = new FlashCard(question, answer, hint, wrongAnswers, tags);
-                    File cardFolder = new File(getContext().getFilesDir(), "flashcards");
-                    card.exportFlash(cardFolder);
-
-                    Bundle b = new Bundle();
-                    b.putSerializable("Card", card);
-                    NavHostFragment.findNavController(Fragment_CreateQuiz.this)
-                            .navigate(R.id.action_fragment_CreateFlashCard_to_fragment_FlashCardVertical, b);
-
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                   //Add Flashcard to Quiz when selected
                 }
-            }
-        });
+            });
+
+            cardView.addView(text);
+            layout.addView(cardView);
+
+        }
+
+        Space space = new Space(getContext());
+        int px = (int) Math.floor(TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP, 75, getResources().getDisplayMetrics()));
+        space.setMinimumHeight(px);
+        layout.addView(space);
+
     }
 }
+
+
+
+
